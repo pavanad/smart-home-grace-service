8cff3b46ddea4396b016787da5a4624169dff601
Adilson Pavan <adilson.pavan@gmail.com>
2024-04-19 00:21:50 -0300

    config HA class

--- app/services/home_assistant.py
+++ app/services/home_assistant.py
@@ -1,2 +1,19 @@
+import os
+
+import requests
+
+
 class HomeAssistantClient:
-    pass
+
+    BASE_URL = "http://192.168.0.3:8123"
+    HA_TOKEN = os.getenv("HOME_ASSISTANT_TOKEN")
+
+    def __init__(self):
+        self.__host = ""
+        self.__header = self.__get_header()
+
+    def __get_header(self) -> dict:
+        return {
+            "Authorization": f"Bearer {self.HA_TOKEN}",
+            "content-type": "application/json",
+        }
