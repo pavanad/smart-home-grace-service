05223a938440bdeab2249248985ab4060b1d2ea8
Adilson Pavan <adilson.pavan@engdb.com.br>
2024-04-19 23:44:27 -0300

    implementation of the get light states

--- app/agent/tools/home.py
+++ app/agent/tools/home.py
@@ -1,9 +1,33 @@
 from langchain.tools import tool
 
+from app.services.home_assistant import HomeAssistantClient
+
 
 @tool
-def smart_home_control():
+def smart_home_lights_state():
     """
-    Useful for when you need to control smart home devices.
+    Useful for when you need to get status of the light devices or entities
+    from the smart home.
     """
-    return "Smart Home Control"
+    results = ""
+    client = HomeAssistantClient()
+
+    filtered_switches = [
+        item
+        for item in client.get_switches()
+        if not any(sub in item["entity_id"] for sub in ["led_", "camera_", "analise_"])
+    ]
+    lights = client.get_lights()
+    lights.extend(filtered_switches)
+
+    for item in lights:
+        results += f"""
+        Name: {item["friendly_name"]}
+        Entity ID: {item["entity_id"]}
+        State: {item["state"]}
+        """
+
+    return f"""
+    Here are the results of the smart home lights state:
+    {results}
+    """
--- app/services/home_assistant.py
+++ app/services/home_assistant.py
@@ -5,15 +5,39 @@ import requests
 
 class HomeAssistantClient:
 
-    BASE_URL = "http://192.168.0.3:8123"
-    HA_TOKEN = os.getenv("HOME_ASSISTANT_TOKEN")
-
     def __init__(self):
-        self.__host = ""
+        self.__host = os.getenv("HOME_ASSISTANT_HOST")
+        self.__token = os.getenv("HOME_ASSISTANT_TOKEN")
         self.__header = self.__get_header()
 
     def __get_header(self) -> dict:
         return {
-            "Authorization": f"Bearer {self.HA_TOKEN}",
+            "Authorization": f"Bearer {self.__token}",
             "content-type": "application/json",
         }
+
+    def __get_states_filter(self, entity: str) -> list:
+        url = f"{self.__host}/api/states"
+        response = requests.get(url, headers=self.__header)
+        data = response.json()
+        entity_list = [
+            {
+                "entity_id": item.get("entity_id"),
+                "state": item.get("state"),
+                "friendly_name": item["attributes"].get("friendly_name"),
+                "last_changed": item.get("last_changed"),
+                "last_updated": item.get("last_updated"),
+            }
+            for item in data
+            if entity in item["entity_id"]
+        ]
+        return entity_list
+
+    def get_lights(self) -> list:
+        return self.__get_states_filter("light.")
+
+    def get_sensors(self) -> list:
+        return self.__get_states_filter("sensor.")
+
+    def get_switches(self) -> list:
+        return self.__get_states_filter("switch.")
